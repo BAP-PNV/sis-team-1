9af7c426ffb57b133e5635455a8574293d273715
DThanh1508 <athanh2002kt@gmail.com>
2022-12-21 17:20:11 +0700

    feat/thanh/migrate_table: Done

--- database/migrations/2022_12_21_061447_create_users_table.php
+++ database/migrations/2022_12_21_061447_create_users_table.php
@@ -14,12 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+//            $table->engine = 'InnoDB';
+//            $table->charset = 'utf8mb4';
+//            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->increments('id');
+            $table->string('username',50);
+            $table->string('email',50);
+            $table->string('password',30);
             $table->timestamps();
         });
     }
