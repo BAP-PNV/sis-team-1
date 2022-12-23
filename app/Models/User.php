a24cf05c7df9b4b629de420657b218f824ade892
DThanh1508 <athanh2002kt@gmail.com>
2022-12-23 08:43:54 +0700

    feat/thanh/make_model: get db from profiles table

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
+    protected $table = 'users';
+    public function profiles(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this -> hasOne(Profile::class,'userId','id');
+    }
+//    public function folders(): \Illuminate\Database\Eloquent\Relations\HasMany
+//    {
+//        return $this -> hasMany(Folder::class, 'ownerId','id');
+//    }
 }
