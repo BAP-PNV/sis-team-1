c99c325517e6142717e62adc8ab1d3b59ee3432b
DThanh1508 <athanh2002kt@gmail.com>
2022-12-23 11:42:37 +0700

    feat/thanh/model: Make User & Profile model

---
+++ app/Models/Profile.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+    protected $table = 'users';
+    public function users(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class,'userId','id');
+    }
+}
