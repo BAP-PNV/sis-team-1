2d42b75b427f5bb4dbc02eb50efde3bb2ae7b6ae
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2022-12-27 15:57:16 +0700

    add images to S3

---
+++ app/Services/Implements/AwsS3Service.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\Implements;
+
+use App\Services\Interfaces\IAwsService;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+
+class AwsS3Service implements IAwsService
+{
+    private string $id = 'user_01/';
+    public function create(UploadedFile $file)
+    {
+        $fileName = time() . '-' . $file->getClientOriginalName();
+        $path =  Storage::disk('s3')->put('laravel/'.$this->id.$fileName,file_get_contents($file));
+        $path = $this->show('laravel/'.$this->id.$fileName);
+        return $path;
+    }
+    public function show(string $url)
+    {
+        $file = Storage::disk('s3')->url($url);
+        return $file;
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignUpController;
@@ -26,3 +27,5 @@ Route::post('login', [AuthController::class,"login"]);
 Route::get('/user-info',[AuthController::class,"getUserInfo"]);
 Route::get("/get-info",[AuthController::class,"confirm"]);
 Route::get("/",[AuthController::class,"confirm"]);
+Route::post('files',[FileController::class,'create']);
+Route::get('file/{id}',[FileController::class,'show']);
