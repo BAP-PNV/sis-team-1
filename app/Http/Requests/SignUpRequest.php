c1c9aa07b41dd728b3ffc6cd771c0db5f8ea5758
hoaingodong <hoaingodong1@gmail.com>
2022-12-27 09:03:12 +0700

    fix sign-up validation

--- app/Http/Requests/SignUpRequest.php
+++ app/Http/Requests/SignUpRequest.php
@@ -24,12 +24,9 @@ class SignUpRequest extends FormRequest
     public function rules()
     {
         return [
-            'username' => 'required|string|max:255',
             'email' => 'required|email',
             'password' => 'required',
-            'password_confirmation' => 'required|same:password',
-            'fullname' => 'required|string|max:255',
-            'avatar_url' => 'required|string',
+            'confirm_password' => 'required|same:password',
         ];
     }
 
