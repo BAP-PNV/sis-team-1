5db77618bc1b74075e0ca1aee107ae7f0037ccb8
DThanh1508 <athanh2002kt@gmail.com>
2022-12-30 13:27:30 +0700

    feat/thanh/make-model: created folder, image tables and fixed key,profile, user table

--- app/Models/Key.php
+++ app/Models/Key.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Key extends Model
 {
     use HasFactory;
-    protected $table = 'keys';
+    protected $table = 'api_keys';
     public function user()
     {
        return $this->belongsTo(User::class);
