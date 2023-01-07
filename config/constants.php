272dc7340fa3a490e03a56002d8f86246bfc41b2
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-01-08 00:08:09 +0700

    fix can not redirect to FE

--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,5 @@
 <?php
 return [
-    's3_url_top' => env('AWS_S3_TOP_URL', 'http://localhost/')
+    's3_url_top' => env('AWS_S3_TOP_URL', 'http://localhost/'),
+    'APP_FE_URL' => env('APP_FE_URL', 'http://localhost:3000')
 ];
